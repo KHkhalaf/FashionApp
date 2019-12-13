2c4edbca51e0af949f9f95e704f1a0f071fe11da
hp <hp@DESKTOP-FKTABFC>
2019-12-13 18:54:04 +0200

    a Commit

---
+++ FashionApp/FashionApp/Views/CollectionsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FashionApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CollectionsPage : ContentPage
+    {
+        public CollectionsPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        public List<Category> MyCollections { get => GetCollections(); }
+
+        private List<Category> GetCollections()
+        {
+            var colList = new List<Category>();
+            colList.Add(new Category { Image = "watches.png", Title = "MEN'S WRISTWATCHES" });
+            colList.Add(new Category { Image = "minidress.png", Title = "WOMEN'S MINI DRESSES" });
+            colList.Add(new Category { Image = "coats.png", Title = "WOMEN'S COATS" });
+            return colList;
+        }
+    }
+
+    public class Category
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Caption { get; set; }
+    }
+}
---
+++ FashionApp/FashionApp/Views/ExplorePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FashionApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExplorePage : ContentPage
+    {
+        public ExplorePage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        public List<Category> AllCategories { get => GetCategories(); }
+
+        private List<Category> GetCategories()
+        {
+            var catList = new List<Category>();
+            catList.Add(new Category { Image = "summerCol.png", Title = "SUMMER COLLECTION", Caption = "BEST DISCOUNT THIS SEASON" });
+            catList.Add(new Category { Image = "womenCol.png", Title = "WOMEN'S CLOTHINGS", Caption = "UP TO 50% OFF ON EVERY ITEM" });
+            catList.Add(new Category { Image = "elegantCol.png", Title = "ELEGANT CLOTHINGS", Caption = "UNQUE COLLECTIONS AND STYLES" });
+            return catList;
+        }
+    }
+}
